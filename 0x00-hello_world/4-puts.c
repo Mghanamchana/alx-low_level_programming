ed795beeb22c56aab337d9958520c662946daf33
Mghanamchana <mghanagilbert@gmail.com>
2023-08-11 17:57:44 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - A programm that shows an entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
